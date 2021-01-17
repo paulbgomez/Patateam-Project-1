99ce5aacd9d66e0fc71c4f1f1d39d1612d24b519
Paul Barroso <paulbg1993@gmail.com>
2021-01-17 13:05:29 +0100

    Create Random Characters Methods

--- FirstWeekAssignment/src/com/ironhack/main/Party.java
+++ FirstWeekAssignment/src/com/ironhack/main/Party.java
@@ -1,31 +1,30 @@
 package com.ironhack.main;
 
+import com.ironhack.Character.Warrior;
+import com.ironhack.Character.Wizard;
+
 import java.util.*;
 
 // FIESTAAAA
 public class Party {
-    private List<Character> npcs;
-    private static List<Character> generatedCharactersFromMenu = new ArrayList<>();
+    private static List<Object> partyTeam1 = new ArrayList<>();
+    private static List<Object> partyTeam2 = new ArrayList<>();
+    private static List<Object> graveYard = new ArrayList<>();
 
 
     //Generate random party size between 0 and 10
-    Random random = new Random();
-    private int randomPartySize = (random.nextInt(10)) + 1;
+    private static Random randomNumber = new Random();
+    private static int randomPartySize = (randomNumber.nextInt(10)) + 1;
 
     // Constructor con número de miembros de party que hace party random
 
     //Constructor vacío full random
-    public Party() {
-        List<Character> npcs = createRandomParty();
-    }
 
     // Constructor con arraylist de miembros
 
-    // static crear party desde el menú
-    public static List<Character> addFromMenu(Character newCharacter) {
-        //private static List<Character> generatedCharactersFromMenu = new ArrayList<>();
-        generatedCharactersFromMenu.add(newCharacter);
-        return generatedCharactersFromMenu;
+    // static crear party desde el menú. Checkear si se puede añadir tanto warrior como wizard
+    public static void addCharacter(Character newCharacter, List<Character> partyTeam) {
+        partyTeam.add(newCharacter);
     }
 
     // send to graveyard (index, graveyard)
@@ -35,24 +34,36 @@ public class Party {
     // static que importe y devuelva la party o Contructor que importe
 
     // getRandom() y get(index)
-    public List<Character> createRandomParty() {
-       int innerParty = getRandomPartySize();
-       for(int i = innerParty; i > 0; i--){
-           i % 2 == 0 ? npcs.add(generateRandomWizard) : npcs.add(generateRandomWarrior);
-       }
-       return npcs;
+    public static void createRandomCharacter(){
+        if(Math.random() > .5){
+            int randomHp = 99 + (int) Math.ceil(Math.random() * 101);
+            int randomStamina = 9 + (int) Math.ceil(Math.random() * 41);
+            int randomStrength = (randomNumber.nextInt(10)) + 1;
+            String randomName; //sacar de array nombres
+            //llamar al metodo estatico de id
+            Warrior randomWarrior = new Warrior(1, "Juan", randomHp, true, randomStamina, randomStrength);
+        } else {
+            int randomHp = 49 + (int) Math.ceil(Math.random() * 51);
+            int randomMana = 9 + (int) Math.ceil(Math.random() * 41);
+            int randomIntelligence = 1 + (int) Math.ceil(Math.random() * 49);
+            String randomName;//sacar de array nombres
+            //llamar al metodo estatico de id
+            Wizard randomWizard = new Wizard(1, "Juan", randomHp, true, randomMana, randomIntelligence);
+        }
     }
 
-    public int getRandomPartySize() {
-        return randomPartySize;
+    public static void createRandomParty(int selectedListFromMenu) {
+        if(selectedListFromMenu == 1){
+            for(int i = 0; i <= randomPartySize; i++){
+                partyTeam1.add(createRandomCharacter());
+            }
+        } else {
+            for(int i = 0; i <= randomPartySize; i++){
+                partyTeam2.add(createRandomCharacter());
+            }
+        }
     }
 
-    public static List<Character> getGeneratedCharactersFromMenu() {
-        return generatedCharactersFromMenu;
-    }
 
-    public List<Character> getNpcs() {
-        return npcs;
-    }
 }
 
