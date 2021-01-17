04a2fdcf1c4cd43ad30ffbb908452e79373f37f6
Paul Barroso <paulbg1993@gmail.com>
2021-01-17 13:09:12 +0100

    Fixed method createRandomCharacter()

--- FirstWeekAssignment/src/com/ironhack/main/Party.java
+++ FirstWeekAssignment/src/com/ironhack/main/Party.java
@@ -34,7 +34,7 @@ public class Party {
     // static que importe y devuelva la party o Contructor que importe
 
     // getRandom() y get(index)
-    public static void createRandomCharacter(){
+    public static Object createRandomCharacter(){
         if(Math.random() > .5){
             int randomHp = 99 + (int) Math.ceil(Math.random() * 101);
             int randomStamina = 9 + (int) Math.ceil(Math.random() * 41);
@@ -42,6 +42,7 @@ public class Party {
             String randomName; //sacar de array nombres
             //llamar al metodo estatico de id
             Warrior randomWarrior = new Warrior(1, "Juan", randomHp, true, randomStamina, randomStrength);
+            return randomWarrior;
         } else {
             int randomHp = 49 + (int) Math.ceil(Math.random() * 51);
             int randomMana = 9 + (int) Math.ceil(Math.random() * 41);
@@ -49,6 +50,7 @@ public class Party {
             String randomName;//sacar de array nombres
             //llamar al metodo estatico de id
             Wizard randomWizard = new Wizard(1, "Juan", randomHp, true, randomMana, randomIntelligence);
+            return randomWizard;
         }
     }
 
