b7f203915d8b5d4e6dab3882fa940dd570f021c0
Paul Barroso <paulbg1993@gmail.com>
2021-01-20 19:43:32 +0100

    ReProper graveyard

--- FirstWeekAssignment/src/com/ironhack/main/Graveyard.java
+++ FirstWeekAssignment/src/com/ironhack/main/Graveyard.java
@@ -19,15 +19,36 @@ public class Graveyard {
     @Override
     public String toString() {
         if(getGraveyardList().size() == 0){
-            return "+++ The Graveyard is empty +++";
+            return "+++ The Graveyard is empty.... +++ \n" +
+                    "              ...                            \u0003\n" +
+                    "             ;::::;                           \u0003\n" +
+                    "           ;::::; :;                          \u0003\n" +
+                    "         ;:::::'   :;                         \u0003\n" +
+                    "        ;:::::;     ;.                        \u0003\n" +
+                    "       ,:::::'       ;           OOO\\         \u0003\n" +
+                    "       ::::::;       ;          OOOOO\\        \u0003\n" +
+                    "       ;:::::;       ;         OOOOOOOO       \u0003\n" +
+                    "      ,;::::::;     ;'         / OOOOOOO      \u0003\n" +
+                    "    ;:::::::::`. ,,,;.        /  / DOOOOOO    \u0003\n" +
+                    "  .';:::::::::::::::::;,     /  /     DOOOO   \u0003\n" +
+                    " ,::::::;::::::;;;;::::;,   /  /        DOOO  \u0003\n" +
+                    ";`::::::`'::::::;;;::::: ,#/  /          DOOO \u0003\n" +
+                    ":`:::::::`;::::::;;::: ;::#  /            DOOO\u0003\n" +
+                    "::`:::::::`;:::::::: ;::::# /              DOO\u0003\n" +
+                    "`:`:::::::`;:::::: ;::::::#/               DOO\u0003\n" +
+                    " :::`:::::::`;; ;:::::::::##                OO\u0003\n" +
+                    " ::::`:::::::`;::::::::;:::#                OO\u0003\n" +
+                    " `:::::`::::::::::::;'`:;::#                O \u0003\n" +
+                    "  `:::::`::::::::;' /  / `:#                  \u0003\n" +
+                    "   ::::::`:::::;'  /  /   `#         FOR NOW.....     ";
         } else {
             String graveyardDeaths = "The Graveyard is filled with the souls of \n";
             for (int i = 0; i < getGraveyardList().size(); i++) {
                 graveyardDeaths += getGraveyardList().get(i).getName() + ", a brave " + getGraveyardList().get(i).getCharacterType()
-                + "            ,-=-.       ______      _\n" +
+                + "\n"  + "    ,-=-.       ______      _\n" +
                         "     /  +  \\    />----->   _|1|_\n" +
-                        "     | ~~~ |    // -/- /   |_ H _|\n" +
-                        "     |R.I.P|   //  /  /      |S|\n" +
+                        "     | ~~~ |    // -/- /  |_ H _|\n" +
+                        "     |R.I.P|   //  /  /     |S|\n" +
                         "\\vV,,|_____|V,//_____/VvV,v,|_|/,,vhjwv/,";
             }
             return graveyardDeaths;
